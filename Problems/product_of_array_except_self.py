9d77993a18e8eb87b1fedb8fe66b6a8c9c19394c
Abhilash Knox George <user.abhilash@gmail.com>
2022-12-13 02:48:30 +0530

    Added unsaved files

--- Problems/product_of_array_except_self.py
+++ Problems/product_of_array_except_self.py
@@ -39,19 +39,46 @@ class Solution(object):
         #     answer.insert(i,int(answers/nums[i]))
 
         #     print(answer)
-        left_prod = []
-        right_prod = []
-        left_prod.insert(0,1)
-        right_prod.insert(len(nums)-1,1)
 
-        for i in range(1,len(nums)):
-            left_prod.insert(i,(nums[i-1]*left_prod[i-1]))
-        for j in reversed(nums):
-            print(j)
-            right_prod.insert(j,(nums[j]))
-        print(left_prod,right_prod)
+# This is complex why do it this way I have no clue yet but seems reasonable but chatGPT explained the solution way better to me than these youtube channel solution 
+# I really need to learn to solve all these problems myself , planning on starting a startup fed up of sitting still in a company I want to use my resources to its full potential.
+
+        # left_prod = []
+        # right_prod = []
+        # left_prod.insert(0,1)
+        # right_prod.insert(len(nums)-1,1)
+
+        # for i in range(1,len(nums)):
+        #     left_prod.insert(i,(nums[i-1]*left_prod[i-1]))
+        # for j in reversed(nums):
+        #     print(j)
+        #     right_prod.insert(j,(nums[j]))
+        # print(left_prod,right_prod)
 
 
+# THE ULTIMATE SOLUTION
+
+        # Create a new array to store the result
+        result = []
+        
+        # Loop through the input array
+        for i in range(len(nums)):
+            # Initialize a variable to store the product
+            # Set it to 1 initially to account for the fact
+            # that we will be multiplying the elements together
+            product = 1
+            
+            # Loop through the array again, excluding the current element
+            for j in range(len(nums)):
+                if i != j:
+                    # Multiply the current element with the product
+                    product *= nums[j]
+            
+            # Append the product to the result array
+            result.append(product)
+        
+        # Return the result array
+        return result
 
 
 obj1 = Solution()
